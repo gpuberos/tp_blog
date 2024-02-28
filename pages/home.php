89fdb9f43bf3c54525dc833a5622c223a16c7dce
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-02-28 16:25:29 +0100

    add: move var_dump

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,10 @@
     <?php foreach ($db->query('SELECT * FROM articles', 'App\Table\Article') as $post) : ?>
         
-        <?php // var_dump($post); ?>
-
+        
         <h2><a href="<?php $post->getURL(); ?>"><?= $post->titre; ?></a></h2>
-
+        
         <p><?php $post->getExtrait(); ?></p>
-
+        
+        <?php var_dump($post); ?>
 
     <?php endforeach; ?>
