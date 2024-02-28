2fc20c8351523d2d3f0af84932ed466dae6835bf
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-02-28 14:26:17 +0100

    add: PDO

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,11 @@
-<h1>Je suis la homepage</h1>
+<?php
 
-<p><a href="index.php?p=single">Single</a></p>
+$pdo = new PDO('mysql:dbname=db_blog;host=localhost', 'root', 'root');
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// $count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date="' . date('Y-m-d H:i:s') . '"');
+
+$res = $pdo->query('SELECT * FROM articles');
+
+var_dump($res->fetchAll(PDO::FETCH_OBJ));
--- pages/templates/default.php
+++ pages/templates/default.php
@@ -8,9 +8,9 @@
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <title>Starter Template for Bootstrap</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>Project Name</title>
+    <meta name="description" content="TP Création d'un blog">
+    <meta name="author" content="Guy-Philippe Uberos">
 
 </head>
 
@@ -18,7 +18,7 @@
     <header>
         <nav class="navbar navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#">Project name</a>
+                <a class="navbar-brand" href="index.php?p=home">Project name</a>
             </div>
         </nav>
     </header>
